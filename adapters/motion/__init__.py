f7d1e65edf3e5c3fcf00366ec7205cd1ced5d290
zouhs <248446746@qq.com>
2026-09-19 00:25:07 +0800

    feat: expose Blender Anime adapter

--- adapters/motion/__init__.py
+++ adapters/motion/__init__.py
@@ -19,3 +19,5 @@ __all__ = [
     "run_smoke",
     "smoke_command",
 ]
+
+from .blender_anime import (\n    BlenderAnimeError,\n    BlenderVersion,\n    discover_binary as discover_blender_binary,\n    inspect_environment as inspect_blender_environment,\n    parse_version as parse_blender_version,\n    smoke_command as blender_smoke_command,\n)\n\n__all__ += [\n    "BlenderAnimeError",\n    "BlenderVersion",\n    "discover_blender_binary",\n    "inspect_blender_environment",\n    "parse_blender_version",\n    "blender_smoke_command",\n]\n
