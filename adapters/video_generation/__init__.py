5d3d5b9a11063c92d5ed7497035000314ed61314
zouhs <248446746@qq.com>
2026-09-23 09:02:23 +0800

    feat(p36): export local micro-motion adapter

--- adapters/video_generation/__init__.py
+++ adapters/video_generation/__init__.py
@@ -2,6 +2,7 @@
 
 from .base import VideoGenerationError, VideoGenerationProvider, VideoGenerationRequest, VideoGenerationResult, normalize_image_paths
 from .local_ken_burns import LocalKenBurnsVideo
+from .local_micro_motion import LocalMicroMotionVideo
 from .minimax_h3 import MiniMaxH3Video
 from .openai_sora import OpenAISoraVideo
 from .runway import RunwayImageToVideo
@@ -10,6 +11,7 @@ from .wan import WanImageToVideo
 
 __all__ = [
     "LocalKenBurnsVideo",
+    "LocalMicroMotionVideo",
     "MiniMaxH3Video",
     "OpenAISoraVideo",
     "RunwayImageToVideo",
